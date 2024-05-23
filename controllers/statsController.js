3e788ccfa4e405cfc7ba8ffcdef45c4e23791527
FaresMagdy99 <83503909+FaresMagdy99@users.noreply.github.com>
2024-05-23 17:34:36 +0300

    fixed stats

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -1,8 +1,9 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 
 exports.getAdminStats = async (req, res) => {
-    
+
     // Verify JWT token, get user ID, and check role (implement middleware)
     const decoded = jwt.verify(req.headers.authorization.split(' ')[1], process.env.JWT_SECRET);
     const userId = decoded.userId;
@@ -10,64 +11,92 @@ exports.getAdminStats = async (req, res) => {
     const user = await User.findOne({ _id: userId })
     // console.log(user);
     if (user.role !== 'ADMIN') {
-      return res.status(401).json({ message: 'Unauthorized: Only admin can access stats' });
+        return res.status(401).json({ message: 'Unauthorized: Only admin can access stats' });
     }
-  
+
     try {
-      const pipeline = [
-        {
-          $lookup: {
-            from: 'PropertyRequest', // Join with PropertyRequest collection
-            localField: '_id', // Local field in User collection (usually _id)
-            foreignField: 'createdBy', // Foreign field referencing User in PropertyRequest
-            as: 'requests',
-          },
-        },
-        {
-          $lookup: {
-            from: 'Ad', // Join with Ad collection
-            localField: '_id', // Local field in User collection (usually _id)
-            foreignField: 'createdBy', // Foreign field referencing User in Ad collection
-            as: 'ads',
-          },
-        },
-        {
-          $unwind: '$requests', // Unwind request documents for aggregation
-        },
-        {
-          $unwind: '$ads', // Unwind ad documents for aggregation
-        },
-        {
-          $group: {
-            _id: {
-              id: '$_id', // Group by user ID
-              role: '$role', // Group by user role
-            },
-            name: { $first: '$name' }, // Get first name from user document
-            requestsCount: { $sum: 1 }, // Count number of requests
-            totalRequestsAmount: { $sum: '$requests.price' }, // Sum request prices
-            adsCount: { $sum: 1 }, // Count number of ads
-            totalAdsAmount: { $sum: '$ads.price' }, // Sum ad prices
-          },
-        },
-        {
-          $group: {
-            _id: null, // Group all documents into one
-            data: { $push: '$$ROOT' }, // Push user stats objects into an array
-          },
-        },
-      ];
-  
-      const stats = await User.aggregate(pipeline);
-  
-      if (!stats.length) {
-        return res.json({ data: [], message: 'No user statistics found' });
-      }
-  
-      res.json(stats[0]); // Return the first element containing the data array
+        const pipeline = [
+            {
+                '$lookup': {
+                    'from': 'propertyrequests',
+                    'localField': '_id',
+                    'foreignField': 'createdBy',
+                    'as': 'requests'
+                }
+            }, {
+                '$lookup': {
+                    'from': 'ads',
+                    'localField': '_id',
+                    'foreignField': 'createdBy',
+                    'as': 'ads'
+                }
+            }, {
+                '$unwind': {
+                    'path': '$requests',
+                    'preserveNullAndEmptyArrays': true
+                }
+            }, {
+                '$unwind': {
+                    'path': '$ads',
+                    'preserveNullAndEmptyArrays': true
+                }
+            }, {
+                '$group': {
+                    '_id': {
+                        'id': '$_id',
+                        'role': '$role'
+                    },
+                    'name': {
+                        '$first': '$name'
+                    },
+                    'requestsCount': {
+                        '$sum': {
+                            '$cond': [
+                                {
+                                    '$not': [
+                                        '$requests'
+                                    ]
+                                }, 0, 1
+                            ]
+                        }
+                    },
+                    'totalRequestsAmount': {
+                        '$sum': '$requests.price'
+                    },
+                    'adsCount': {
+                        '$sum': {
+                            '$cond': [
+                                {
+                                    '$not': [
+                                        '$ads'
+                                    ]
+                                }, 0, 1
+                            ]
+                        }
+                    },
+                    'totalAdsAmount': {
+                        '$sum': '$ads.price'
+                    }
+                }
+            }, {
+                '$group': {
+                    '_id': null,
+                    'data': {
+                        '$push': '$$ROOT'
+                    }
+                }
+            }
+        ];
+
+        const stats = await User.aggregate(pipeline);
+
+        if (!stats.length) {
+            return res.json({ data: [], message: 'No user statistics found' });
+        }
+
+        res.json(stats[0]); // Return the first element containing the data array
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal server error' });
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
     }
-  };
-  
+};
